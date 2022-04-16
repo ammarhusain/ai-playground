b262304be4e286d69991abe623267fe481505658
Ammar Husain (macbook) <ammarh@google.com>
2022-04-15 17:02:28 -0700

    starting ammar-bot

--- app.py
+++ app.py
@@ -24,16 +24,18 @@ def index():
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return """Marv is a chatbot that reluctantly answers questions with sarcastic responses:
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Robot
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+You: How many pounds are in a kilogram?
+Marv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.
+You: What does HTML stand for?
+Marv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.
+You: When did the first airplane fly?
+Marv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.
+You: What is the meaning of life?
+Marv: I’m not sure. I’ll ask my friend Google.
+You: Why is the sky blue?
+Marv: """
 
 if __name__ == "__main__":
     app.run()
